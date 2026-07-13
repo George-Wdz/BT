99f5cc6759d3d8d23445f88b5d6f4f6508ab72a1
kkkrizalid <3511697658@qq.com>
2026-07-13 13:41:35 +0800

    Add utility functions for model training and evaluation
    Added utility functions for model training, validation, and testing, including learning rate adjustment, early stopping, and data conversion. Implemented visualization and performance metrics calculation.

---
+++ Time and Space/GPT4TS/utils/tools.py
@@ -0,0 +1,409 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import os
+
+from datetime import datetime
+from distutils.util import strtobool
+import pandas as pd
+
+from utils.metrics import metric
+
+plt.switch_backend('agg')
+
+def adjust_learning_rate(optimizer, epoch, args):
+    # lr = args.learning_rate * (0.2 ** (epoch // 2))
+    # if args.decay_fac is None:
+    #     args.decay_fac = 0.5
+    # if args.lradj == 'type1':
+    #     lr_adjust = {epoch: args.learning_rate * (args.decay_fac ** ((epoch - 1) // 1))}
+    # elif args.lradj == 'type2':
+    #     lr_adjust = {
+    #         2: 5e-5, 4: 1e-5, 6: 5e-6, 8: 1e-6,
+    #         10: 5e-7, 15: 1e-7, 20: 5e-8
+    #     }
+    if args.lradj =='type1':
+        lr_adjust = {epoch: args.learning_rate if epoch < 3 else args.learning_rate * (0.9 ** ((epoch - 3) // 1))}
+    elif args.lradj =='type2':
+        lr_adjust = {epoch: args.learning_rate * (args.decay_fac ** ((epoch - 1) // 1))}
+    elif args.lradj =='type4':
+        lr_adjust = {epoch: args.learning_rate * (args.decay_fac ** ((epoch) // 1))}
+    else:
+        args.learning_rate = 1e-4
+        lr_adjust = {epoch: args.learning_rate if epoch < 3 else args.learning_rate * (0.9 ** ((epoch - 3) // 1))}
+    print("lr_adjust = {}".format(lr_adjust))
+    if epoch in lr_adjust.keys():
+        lr = lr_adjust[epoch]
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+        print('Updating learning rate to {}'.format(lr))
+
+
+class EarlyStopping:
+    def __init__(self, patience=7, verbose=False, delta=0):
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+
+    def __call__(self, val_loss, model, path):
+        score = -val_loss
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model, path)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model, path)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model, path):
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), path + '/' + 'checkpoint.pth')
+        self.val_loss_min = val_loss
+
+
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+class StandardScaler():
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def transform(self, data):
+        return (data - self.mean) / self.std
+
+    def inverse_transform(self, data):
+        return (data * self.std) + self.mean
+
+
+def visual(true, preds=None, name='./pic/test.pdf'):
+    """
+    Results visualization
+    """
+    plt.figure()
+    plt.plot(true, label='GroundTruth', linewidth=2)
+    if preds is not None:
+        plt.plot(preds, label='Prediction', linewidth=2)
+    plt.legend()
+    plt.savefig(name, bbox_inches='tight')
+
+
+def convert_tsf_to_dataframe(
+    full_file_path_and_name,
+    replace_missing_vals_with="NaN",
+    value_column_name="series_value",
+):
+    col_names = []
+    col_types = []
+    all_data = {}
+    line_count = 0
+    frequency = None
+    forecast_horizon = None
+    contain_missing_values = None
+    contain_equal_length = None
+    found_data_tag = False
+    found_data_section = False
+    started_reading_data_section = False
+
+    with open(full_file_path_and_name, "r", encoding="cp1252") as file:
+        for line in file:
+            # Strip white space from start/end of line
+            line = line.strip()
+
+            if line:
+                if line.startswith("@"):  # Read meta-data
+                    if not line.startswith("@data"):
+                        line_content = line.split(" ")
+                        if line.startswith("@attribute"):
+                            if (
+                                len(line_content) != 3
+                            ):  # Attributes have both name and type
+                                raise Exception("Invalid meta-data specification.")
+
+                            col_names.append(line_content[1])
+                            col_types.append(line_content[2])
+                        else:
+                            if (
+                                len(line_content) != 2
+                            ):  # Other meta-data have only values
+                                raise Exception("Invalid meta-data specification.")
+
+                            if line.startswith("@frequency"):
+                                frequency = line_content[1]
+                            elif line.startswith("@horizon"):
+                                forecast_horizon = int(line_content[1])
+                            elif line.startswith("@missing"):
+                                contain_missing_values = bool(
+                                    strtobool(line_content[1])
+                                )
+                            elif line.startswith("@equallength"):
+                                contain_equal_length = bool(strtobool(line_content[1]))
+
+                    else:
+                        if len(col_names) == 0:
+                            raise Exception(
+                                "Missing attribute section. Attribute section must come before data."
+                            )
+
+                        found_data_tag = True
+                elif not line.startswith("#"):
+                    if len(col_names) == 0:
+                        raise Exception(
+                            "Missing attribute section. Attribute section must come before data."
+                        )
+                    elif not found_data_tag:
+                        raise Exception("Missing @data tag.")
+                    else:
+                        if not started_reading_data_section:
+                            started_reading_data_section = True
+                            found_data_section = True
+                            all_series = []
+
+                            for col in col_names:
+                                all_data[col] = []
+
+                        full_info = line.split(":")
+
+                        if len(full_info) != (len(col_names) + 1):
+                            raise Exception("Missing attributes/values in series.")
+
+                        series = full_info[len(full_info) - 1]
+                        series = series.split(",")
+
+                        if len(series) == 0:
+                            raise Exception(
+                                "A given series should contains a set of comma separated numeric values. At least one numeric value should be there in a series. Missing values should be indicated with ? symbol"
+                            )
+
+                        numeric_series = []
+
+                        for val in series:
+                            if val == "?":
+                                numeric_series.append(replace_missing_vals_with)
+                            else:
+                                numeric_series.append(float(val))
+
+                        if numeric_series.count(replace_missing_vals_with) == len(
+                            numeric_series
+                        ):
+                            raise Exception(
+                                "All series values are missing. A given series should contains a set of comma separated numeric values. At least one numeric value should be there in a series."
+                            )
+
+                        all_series.append(pd.Series(numeric_series).array)
+
+                        for i in range(len(col_names)):
+                            att_val = None
+                            if col_types[i] == "numeric":
+                                att_val = int(full_info[i])
+                            elif col_types[i] == "string":
+                                att_val = str(full_info[i])
+                            elif col_types[i] == "date":
+                                att_val = datetime.strptime(
+                                    full_info[i], "%Y-%m-%d %H-%M-%S"
+                                )
+                            else:
+                                raise Exception(
+                                    "Invalid attribute type."
+                                )  # Currently, the code supports only numeric, string and date types. Extend this as required.
+
+                            if att_val is None:
+                                raise Exception("Invalid attribute value.")
+                            else:
+                                all_data[col_names[i]].append(att_val)
+
+                line_count = line_count + 1
+
+        if line_count == 0:
+            raise Exception("Empty file.")
+        if len(col_names) == 0:
+            raise Exception("Missing attribute section.")
+        if not found_data_section:
+            raise Exception("Missing series information under data section.")
+
+        all_data[value_column_name] = all_series
+        loaded_data = pd.DataFrame(all_data)
+
+        return (
+            loaded_data,
+            frequency,
+            forecast_horizon,
+            contain_missing_values,
+            contain_equal_length,
+        )
+
+
+def vali(model, vali_data, vali_loader, criterion, args, device, itr):
+    total_loss = []
+    if args.model == 'PatchTST' or args.model == 'DLinear' or args.model == 'TCN':
+        model.eval()
+    else:
+        model.in_layer.eval()
+        model.out_layer.eval()
+    with torch.no_grad():
+        for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in tqdm(enumerate(vali_loader)):
+            batch_x = batch_x.float().to(device)
+            batch_y = batch_y.float()
+
+            batch_x_mark = batch_x_mark.float().to(device)
+            batch_y_mark = batch_y_mark.float().to(device)
+
+            outputs = model(batch_x, itr)
+            
+            # encoder - decoder
+            outputs = outputs[:, -args.pred_len:, :]
+            batch_y = batch_y[:, -args.pred_len:, :].to(device)
+
+            pred = outputs.detach().cpu()
+            true = batch_y.detach().cpu()
+
+            loss = criterion(pred, true)
+
+            total_loss.append(loss)
+    total_loss = np.average(total_loss)
+    if args.model == 'PatchTST' or args.model == 'DLinear' or args.model == 'TCN':
+        model.train()
+    else:
+        model.in_layer.train()
+        model.out_layer.train()
+    return total_loss
+
+def MASE(x, freq, pred, true):
+    masep = np.mean(np.abs(x[:, freq:] - x[:, :-freq]))
+    return np.mean(np.abs(pred - true) / (masep + 1e-8))
+
+def test(model, test_data, test_loader, args, device, itr):
+    preds = []
+    trues = []
+    # mases = []
+
+    # Get feature names from dataset
+    feature_names = getattr(test_data, 'feature_names', None)
+    if feature_names is None:
+        # Fallback to generic names if not available
+        feature_names = [f'feature_{i}' for i in range(args.c_out)]
+
+    model.eval()
+    with torch.no_grad():
+        for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in tqdm(enumerate(test_loader)):
+
+            # outputs_np = batch_x.cpu().numpy()
+            # np.save("emb_test/ETTh2_192_test_input_itr{}_{}.npy".format(itr, i), outputs_np)
+            # outputs_np = batch_y.cpu().numpy()
+            # np.save("emb_test/ETTh2_192_test_true_itr{}_{}.npy".format(itr, i), outputs_np)
+
+            batch_x = batch_x.float().to(device)
+            batch_y = batch_y.float()
+
+            outputs = model(batch_x[:, -args.seq_len:, :], itr)
+
+            # encoder - decoder
+            outputs = outputs[:, -args.pred_len:, :]
+            batch_y = batch_y[:, -args.pred_len:, :].to(device)
+
+            pred = outputs.detach().cpu().numpy()
+            true = batch_y.detach().cpu().numpy()
+
+            preds.append(pred)
+            trues.append(true)
+
+    preds = np.concatenate(preds, axis=0)
+    trues = np.concatenate(trues, axis=0)
+    # mases = np.mean(np.array(mases))
+    print('test shape:', preds.shape, trues.shape)
+
+    # Save detailed predictions and ground truth
+    folder_path = './results/' + args.model_id + '/'
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    # Save as numpy arrays (normalized)
+    np.save(folder_path + f'predictions_itr{itr}.npy', preds)
+    np.save(folder_path + f'groundtruth_itr{itr}.npy', trues)
+
+    # Inverse transform to get real values (denormalize)
+    # Channel-independent layout: samples are ordered by feat_id = index // tot_len,
+    # so each feature owns a contiguous block of `tot_len` windows.
+    # drop_last in DataLoader can truncate the last block.
+    num_features = len(feature_names)
+    per_feat_total = test_data.tot_len
+    total_samples = preds.shape[0]
+
+    preds_denorm = np.zeros_like(preds)
+    trues_denorm = np.zeros_like(trues)
+
+    has_scaler = hasattr(test_data, 'scaler') and hasattr(test_data.scaler, 'mean_')
+    if has_scaler:
+        for feat_idx in range(num_features):
+            start = feat_idx * per_feat_total
+            if start >= total_samples:
+                break
+            end = min(start + per_feat_total, total_samples)
+            mean_val = test_data.scaler.mean_[feat_idx]
+            scale_val = test_data.scaler.scale_[feat_idx]
+            preds_denorm[start:end, :, 0] = preds[start:end, :, 0] * scale_val + mean_val
+            trues_denorm[start:end, :, 0] = trues[start:end, :, 0] * scale_val + mean_val
+    else:
+        preds_denorm = preds.copy()
+        trues_denorm = trues.copy()
+
+    # Save denormalized arrays
+    np.save(folder_path + f'predictions_denorm_itr{itr}.npy', preds_denorm)
+    np.save(folder_path + f'groundtruth_denorm_itr{itr}.npy', trues_denorm)
+
+    # Save per-feature comparison CSVs
+    summary_df = pd.DataFrame()
+    for feat_idx, feature_name in enumerate(feature_names):
+        start = feat_idx * per_feat_total
+        if start >= total_samples:
+            break
+        end = min(start + per_feat_total, total_samples)
+
+        feat_preds = preds_denorm[start:end, :, 0].flatten()
+        feat_trues = trues_denorm[start:end, :, 0].flatten()
+
+        feat_df = pd.DataFrame({
+            f'{feature_name}_真实值': feat_trues,
+            f'{feature_name}_预测值': feat_preds,
+            f'{feature_name}_误差': np.abs(feat_trues - feat_preds),
+            f'{feature_name}_相对误差(%)': np.abs((feat_trues - feat_preds) / (feat_trues + 1e-8)) * 100
+        })
+
+        # Sanitize feature name for filename
+        safe_name = feature_name.replace('(', '_').replace(')', '').replace('/', '_').replace('%', 'pct').replace('°', 'deg').replace('℃', 'C')
+        feat_df.to_csv(folder_path + f'comparison_{safe_name}_itr{itr}.csv', index=False)
+
+        # Add to summary
+        summary_df.loc[feature_name, 'MAE'] = np.mean(np.abs(feat_trues - feat_preds))
+        summary_df.loc[feature_name, 'RMSE'] = np.sqrt(np.mean((feat_trues - feat_preds)**2))
+        summary_df.loc[feature_name, 'MAPE(%)'] = np.mean(np.abs((feat_trues - feat_preds) / (feat_trues + 1e-8))) * 100
+        summary_df.loc[feature_name, 'Max_Error'] = np.max(np.abs(feat_trues - feat_preds))
+        summary_df.loc[feature_name, 'Mean_True'] = np.mean(feat_trues)
+        summary_df.loc[feature_name, 'Mean_Pred'] = np.mean(feat_preds)
+        summary_df.loc[feature_name, 'Samples'] = end - start
+
+    summary_df.to_csv(folder_path + f'summary_statistics_itr{itr}.csv')
+    print(f'Saved predictions to {folder_path}')
+    print(f'Per-feature windows: {per_feat_total}, drop_last truncated to: {total_samples}, features: {num_features}')
+
+    mae, mse, rmse, mape, mspe, smape, nd = metric(preds, trues)
+    # print('mae:{:.4f}, mse:{:.4f}, rmse:{:.4f}, smape:{:.4f}, mases:{:.4f}'.format(mae, mse, rmse, smape, mases))
+    print('mae:{:.4f}, mse:{:.4f}, rmse:{:.4f}, smape:{:.4f}'.format(mae, mse, rmse, smape))
+
+    return mse, mae
